92f154fb250276e37938834fcd18cfdc564248d4
gugagio <62719586+gugagio@users.noreply.github.com>
2023-12-26 10:44:31 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+console.log('test')
+
+const age1 = 18
+const age2 = 15
+
+if(18<=15){
+    console.log('momxmareblis asaki naklebia an toli 18 ze')
+}else{
+    console.log('momxmareblis asaki aris 18 an zemot' )
+}
+
+
+
+
+
+
+
+
+
+const weekday = 8
+switch (weekday){
+    case 1:
+        console.log('monday')
+        break
+        case 2:
+            console.log('tuesday')
+            break
+            case 3:
+                console.log('wednesday')
+                break
+                case 4:
+                    console.log('thursday')
+                    break
+                    case 5:
+                        console.log('friday')
+                        break
+                        case 6:
+                            console.log('saturday')
+                            break
+                            case 7:
+                                console.log('sunday')
+                                break
+                                case 8:
+                                  
+                                default:
+                                    console.log(' this weekday is not correct')
+}
+
+                        
+
+
+                    
+
+ 
+    
